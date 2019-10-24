4c4f361a5b80dd081c8573be9d5ce7651e0c84a3
DJ <dexter13576@gmail.com>
2019-10-24 13:03:55 +0900

    画像データ送受信実装中

--- python/client.py
+++ python/client.py
@@ -7,35 +7,41 @@ class Client:
 		self.address = address
 		self.port = port
 
+	def send_img(self, sock, img):
+		img_raw = img.tostring()
+		sock.send(img)
+
+
 	def start(self):
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.connect((self.address, self.port))
 		print("Connected:{} {}".format(self.address, self.port))
 
+		"""
 		while True:
 			msg = input("Enter message | Exit(exit): ")
 			if msg == "exit":
 				break
 			sock.send(msg.encode('utf-8'))
 		"""
+
 		capture = cv2.VideoCapture(0)
 		print("Camera open")
 		while True:
 			ret, frame = capture.read()
-			if ret == NULL:
+			if ret == None:
 				continue
 
-			cv2.imshow("barcode", frame)
-
+			#cv2.imshow("barcode", frame)
+			self.send_img(sock, frame)
+			print("send img:{}".format(len(frame)))
 
 			k = cv2.waitKey(1)
 			if k == 27:
 				break
 
-		sock.close()
 		capture.release()
-		cv2.destroyAllWindows()
-		"""
+		#cv2.destroyAllWindows()
 		sock.close()
 		print("Disconnect")
 
--- python/server.py
+++ python/server.py
@@ -1,6 +1,13 @@
 import socket
 import threading
 import sys
+import numpy as np
+import cv2
+
+BUFFER_IMG = 480 * 640 * 3
+BUFFER_MSG = 13
+
+MAX_ACCEPT = 10
 
 class Server:
 	def __init__(self, port):
@@ -10,28 +17,45 @@ class Server:
 
 	def remove_connection(self, con, address):
 		con.close()
-		print("Disconnect:{}".format(address))
+		print("[-]Disconnect:{}".format(address))
 		self.clients.remove((con, address))
 
 	def start(self):
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.bind((self.address, self.port))
-		sock.listen(10)
+		sock.listen(MAX_ACCEPT)
 		print("Start server {}:{}".format(self.address, self.port))
 
 		while True:
 			con, address = sock.accept()
-			print("Connected:{}".format(address))
+			print("[+]Connected:{}".format(address))
 			self.clients.append((con, address))
 			handle_thread = threading.Thread(target=self.handler,
 											args=(con, address),
 											daemon=True)
 			handle_thread.start()
 
-	def handler(self, con, address):
+	def read_msg(self, con, address):
 		while True:
 			try:
-				data = con.recv(1024)
+				data = con.recv(BUFFER_MSG)
+			except ConnectionResetError:
+				self.remove_connection(con, address)
+				break
+			else:
+				if not data:
+					self.remove_connection(con, address)
+					break
+				else:					
+					msg = data.decode('utf-8')
+					print("Recieve:{} - {}".format(address, msg))
+					width, height, size = msg.split(",")
+					return int(size)
+
+	def read_img(self, con, address):
+		while True:
+			try:
+				data = con.recv(BUFFER_IMG)
 			except ConnectionResetError:
 				self.remove_connection(con, address)
 				break
@@ -40,8 +64,15 @@ class Server:
 					self.remove_connection(con, address)
 					break
 				else:
-					msg = data.decode("utf-8")
-					print("Recieve:{} - {}".format(address, msg))
+					img_raw = np.frombuffer(data, dtype=np.uint8)
+					img = np.reshape(img_raw, (480, 640, 3))				
+					return img
+			
+
+	def handler(self, con, address):		
+		#self.read_msg(con, address)
+		img = self.read_img(con, address)
+		cv2.imshow("server:{}".format(address), img)
 
 def main():
 	argv = sys.argv
