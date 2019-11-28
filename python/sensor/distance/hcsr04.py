cb248961fccd920ff794ffc592e70142b4630b65
ehime123 <58251253+ehime123@users.noreply.github.com>
2019-11-28 16:11:02 +0900

    sleepの問題でした

--- python/sensor/distance/hcsr04.py
+++ python/sensor/distance/hcsr04.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+import time
+import RPi.GPIO as GPIO
+
+class sensor:
+    def __init__(self):
+        self.TRIG = 11
+        self.ECHO = 13
+
+    def setup(self):
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+
+        GPIO.setup(self.TRIG, GPIO.OUT)
+        GPIO.setup(self.ECHO, GPIO.IN)
+        GPIO.output(self.TRIG, GPIO.LOW)
+
+        time.sleep(0.1)
+
+        GPIO.output(self.TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(self.TRIG, False)
+
+    def exe(self):
+        while GPIO.input(self.ECHO) == 0:
+            signaloff = time.time()
+
+        while GPIO.input(self.ECHO) == 1:
+            signalon = time.time()
+
+        timepassed = signalon - signaloff
+        print("time span:{}".format(timepassed))
+        GPIO.cleanup()
+        return timepassed * 17000
+
+
+"""
 def reading(sensor):
     import time
     import RPi.GPIO as GPIO
@@ -27,14 +62,21 @@ def reading(sensor):
           signalon = time.time()
  
         timepassed = signalon - signaloff
+        print("timepassed = " + str(timepassed))
         distance = timepassed * 17000
         return distance
         GPIO.cleanup()
+
     else:
         print("Incorrect usonic() function varible.")
+"""
 
 
+def main():
+    s = sensor()
+    while True:
+        s.setup()
+        print(s.exe())
 
-while True:
-	print("{}%".format(reading(0)) + 'timepassed = ' + timepassed)
-
+if __name__ == '__main__':
+    main()
