6fd1ff0ef2a4d9fbf5a34530cf5007b404567553
ehime123 <58251253+ehime123@users.noreply.github.com>
2019-12-05 16:05:59 +0900

    超音波センサ統合版

--- python/client.py
+++ python/client.py
@@ -1,12 +1,17 @@
 import cv2
 import sys
 import socket
+import time
 import datetime
+import threading
+import hcsr04
 
 class Client:
 	def __init__(self, address, port):
 		self.address = address
 		self.port = port
+		self.sensor = hcsr04.sensor()
+		self.flag = 0
 
 	def send_img(self, sock, img):
 		img_raw = img.tostring()
@@ -18,6 +23,11 @@ class Client:
 		sys.stdout.write("\r[{}]send msg:{}".format(now, size))
 		sys.stdout.flush()
 		
+	def handler(self):
+		while True:
+			self.flag = self.sensor.get_flag()
+
+
 	def start(self):
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		sock.connect((self.address, self.port))
@@ -25,15 +35,21 @@ class Client:
 
 		capture = cv2.VideoCapture(0)
 		print("Camera open")
-		while True:
-			ret, frame = capture.read()
-			if ret == None:
-				continue
-			self.send_img(sock, frame)
+		handle_thread = threading.Thread(target=self.handler, daemon=True)
+		handle_thread.start()
 
-			k = cv2.waitKey(1)
-			if k == 27:
-				break
+		while True:
+			print("Flag:{} Dis:{}".format(self.flag, self.sensor.dis))
+			if self.flag == 1:
+				#print("Flag:{} Dis:{}".format(self.flag, self.sensor.dis))
+				for i in range(6):
+					ret, frame = capture.read()
+					if ret == None:
+						continue
+					self.send_img(sock, frame)
+					time.sleep(0.5)
+
+
 
 		capture.release()
 		cv2.destroyAllWindows()
--- python/sensor/distance/hcsr04.py
+++ python/sensor/distance/hcsr04.py
@@ -3,10 +3,15 @@
 import time
 import RPi.GPIO as GPIO
 
+RANGE = 3
+
 class sensor:
     def __init__(self):
         self.TRIG = 11
         self.ECHO = 13
+        self.sum = 0
+        self.avg = 0
+        self.dis = 0
 
     def setup(self):
         GPIO.setwarnings(False)
@@ -30,76 +35,24 @@ class sensor:
             signalon = time.time()
 
         timepassed = signalon - signaloff
-        print("time span:{}".format(timepassed))
+        #print("time span:{}".format(timepassed))
         GPIO.cleanup()
         return timepassed * 17000
 
+    def get_flag(self):
+        flag = -1
+        cnt = 0
+        self.sum = 0
+        while cnt < 15:
+            self.setup()
+            self.dis = self.exe()
+            self.sum = self.sum + self.dis
+            cnt = cnt + 1
 
-
-"""
-def reading(sensor):
-    import time
-    import RPi.GPIO as GPIO
-    GPIO.setwarnings(False)
-     
-    GPIO.setmode(GPIO.BOARD)
-    TRIG = 11
-    ECHO = 13
-     
-    if sensor == 0:
-        GPIO.setup(TRIG,GPIO.OUT)
-        GPIO.setup(ECHO,GPIO.IN)
-        GPIO.output(TRIG, GPIO.LOW)
-        time.sleep(0.3)
-         
-        GPIO.output(TRIG, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG, False)
- 
-        while GPIO.input(ECHO) == 0:
-          signaloff = time.time()
-         
-        while GPIO.input(ECHO) == 1:
-          signalon = time.time()
- 
-        timepassed = signalon - signaloff
-        print("timepassed = " + str(timepassed))
-        distance = timepassed * 17000
-        return distance
-        GPIO.cleanup()
-
-    else:
-        print("Incorrect usonic() function varible.")
-"""
-
-
-def main():
-    s = sensor()
-    sum = 0
-    avg = 0
-    cnt = 0
-    flag = 0
-
-    while True:
-        s.setup()
-        dis = s.exe()
-        if cnt < 15:
-            sum = sum + dis
-        elif cnt == 15:
-            avg = float(sum / 15)
-        cnt = cnt + 1
-
-        print("avg:{}".format(avg))
-        print(dis)
-
-
-        if dis < float(avg - 2):
+        self.avg = float(self.sum / 15)
+        if self.dis < float(self.avg - RANGE):
             flag = 1
         else:
             flag = 0
 
-        print("flag:{}".format(flag))
-
-
-if __name__ == '__main__':
-    main()
+        return flag
