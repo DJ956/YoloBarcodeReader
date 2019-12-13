6ac01b179bd12a7a2cb5e47575d7ea127ed90b5d
DJ <dexter13576@gmail.com>
2019-12-13 14:53:20 +0900

    テストプログラム作成

--- python/convert.py
+++ python/convert.py
@@ -5,30 +5,45 @@ import glob
 import numpy as np
 from pyzbar.pyzbar import decode
 
+#resize gray 59%
+#gray 62%
+#resize gray binary 6.9%
+#gray binary 35%
 
 def exe(folder):
 	scale = 1.5
+	os.remove("result.txt")
+
 	with open("result.txt", "a") as f:
-		for file in glob.glob(folder, "*.png"):
+		files = glob.glob(folder + os.sep + "*.png")
+		count = 0
+		for file in files:
 			img = cv2.imread(file)
 			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-			gray = cv2.resize(gray, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+			#gray = cv2.resize(gray, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
 
 			binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
 				cv2.THRESH_BINARY, 11, 2)
 
+			txt = ""
 			try:
-				result = decode(gray)
-				txt = "{}:{}".format(os.basename(file), result)
+				result = decode(binary)
+				txt = "{}:{}".format(os.path.basename(file), result[0][0])
 				f.write(txt)
+				count += 1
 			except Exception as e:
-				txt = "{}:{}".format(os.basename(file), "failed")
+				txt = "{}:{}".format(os.path.basename(file), "failed")
 				f.write(txt)
 			finally:
 				print(txt)
 				f.write("\n")
 		f.flush()
+
+	per = count / len(files) * 100
+	print("-"*100)
+	print("{}/{}".format(count, len(files)))
+	print("{}%".format(per))
 
 
 
@@ -38,7 +53,7 @@ def main():
 		print("Usage: python {} folder".format(argv[0]))
 		quit(-1)
 
-	exe(folder)
+	exe(argv[1])
 
 if __name__ == "__main__":
 	main()
