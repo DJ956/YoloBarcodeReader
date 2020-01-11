25c09680b835d91106404b25f4ef5481b81e3b40
DJ <dexter13576@gmail.com>
2020-01-12 03:18:13 +0900

    tesseract-ocr検証実験 結果はtesseract識別精度.txtにある通り
    結論:近距離ならpyzbarに画像をそのまま投げたほうが一番制度が出る。
    距離が離れてバーコードがぼやけるようならばtesseract-ocrに変更したほうが良い。
    (*以下のデータは動画ファイルIMG_0009.MOVから画像にしたものである。画像データはGithubのtest_img.zip)

--- python/test_ocr.py
+++ python/test_ocr.py
@@ -2,6 +2,7 @@ import cv2
 import sys
 import os
 import glob
+import time
 import numpy as np
 from PIL import Image
 from pyzbar.pyzbar import decode
@@ -25,18 +26,16 @@ def cv2pil(img):
 	new_img = Image.fromarray(new_img)
 	return new_img
 
-def read_num(img):
-	tools = pyocr.get_available_tools()
-	if len(tools) == 0:
-		print("Not found availables ocr tools")
-		quit(-1)
-
-	tool = tools[0]
+def read_num(img, tool):
 	txt = tool.image_to_string(
 		cv2pil(img),
-		builder=pyocr.builders.TextBuilder(tesseract_layout=6))
+		builder=pyocr.tesseract.DigitBuilder())
 
-	return len(txt) > 0
+	txt = txt.strip()
+	txt = txt.replace('-', '')
+	txt = txt.replace('.', '')
+
+	return len(txt) > 0, txt
 
 
 def cut_barcode(img, detections, is_gray):
@@ -49,44 +48,90 @@ def cut_barcode(img, detections, is_gray):
 		h = point[3] + int(point[3] * scale)	
 
 		cut_img = img[y:h, x:w]
+
+		h,w,c = cut_img.shape
+
+		if h == 0 or w == 0:
+			return None
+
 		if is_gray:
 			cut_img = cv2.cvtColor(cut_img, cv2.COLOR_BGR2GRAY)
-
 		return cut_img
 
 def cut_num(img):
 	start = 0.75
 	h,w = img.shape
 
-	start = int(start * h)
-	cut = img[start:h, 0:w]
-
-	return cut
+	#幅より高さのほうが大きかった場合数字は左右のどちらか
+	if h > w:
+		start = int((1 - start) * w)
+		cut = img[0:h, 0:start]
+		cut = cv2.rotate(cut, cv2.ROTATE_90_COUNTERCLOCKWISE)
+		return cut
+	else:#逆の場合数字は上下のどちらか(この場合下しか想定していない)
+		start = int(start * h)
+		cut = img[start:h, 0:w]
+		return cut
 
 def exe_yolo(files):
+	tools = pyocr.get_available_tools()
+	if len(tools) == 0:
+		print("Not found tools")
+		quit(-1)
+	tool = tools[0]
+
 	yolo = darknet_video.YOLO(CFG, WEIGHT, META)
 	yolo.start_yolo()
 
+	start = time.time()
+
 	count = 0
 	idx = 0
-	for file in files:
-		img = cv2.imread(file)
-		detections, resize_img = yolo.run_yolo(img)
-		if len(detections) > 0:
-			barcode = cut_barcode(resize_img, detections, True)
-			num_img = cut_num(barcode)
+	ANSW = "4901121172880"
+	if os.path.exists("result.txt"):
+		os.remove("result.txt")
 
-			cv2.imwrite("./cut/{}.png".format(idx), num_img)
+	with open("result.txt", "a") as f:
+		for file in files:
+			img = cv2.imread(file)
+			detections, resize_img = yolo.run_yolo(img)
+			if len(detections) > 0:
+				barcode = cut_barcode(resize_img, detections, True)
+				if barcode is None:
+					continue
+				num_img = cut_num(barcode)
 
-			#flag = read_num(num_img)
-			#if flag:
-			#	count += 1
-		idx += 1
+				gamma = 1.8
+				imax = num_img.max()
+				num_img = imax * (num_img / imax) ** (1/gamma)
+
+				cv2.imwrite("./cut/{}".format(os.path.basename(file)), num_img)
+
+				flag, num = read_num(num_img, tool)
+				#桁数が12だった場合、先頭に4を追加する
+				if len(num) == 12 and num[0] != "4":
+					 num = "4{}".format(num)
+				#桁数が13だった場合、先頭の1桁を削除する
+				if len(num) > 13:
+					num = num[1:]
+
+				if flag and num == ANSW:
+					count += 1
+				f.write("Code:{} - File:{}\n".format(num, file))
+
+				sys.stdout.write("\r[{}/{}]".format(idx, len(files)))
+				sys.stdout.flush()
+
+			idx += 1
+
+	end = time.time()
 
 	per = count / len(files) * 100
 	print("-" *  100)
 	print("{}/{}".format(count, len(files)))
 	print("{}%".format(per))
+
+	print("time:{}s".format(end - start))
 
 
 def main():
